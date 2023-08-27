3e2ea07584855f91e0349865cbd43e7607dabb47
JakeRoyRandall <Jake@JakeRandall.me>
2023-08-27 15:29:16 -0400

    new github login button and additional site styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,17 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import AuthButtonClient from "../auth-button";
+import GitHubButton from "./github-button";
 
 export default async function Login() {
   const supabase = createServerComponentClient<Database>({ cookies })
   const { data: { session }} = await supabase.auth.getSession()
   if (session) { redirect('/') }
 
-  return <AuthButtonClient session={session} />
+  return (
+    <div className="flex-1 flex justify-center items-center">
+      <GitHubButton />
+    </div>
+  )
 
 }
